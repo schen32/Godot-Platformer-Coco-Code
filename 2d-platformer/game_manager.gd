b3644f9afe905d2b355d98c8ec0d02d97873e452
Simon Chen <simonc8192@gmail.com>
2025-04-10 15:32:10 -0700

    added main menu and level 2

--- 2d-platformer/game_manager.gd
+++ 2d-platformer/game_manager.gd
@@ -1,7 +1,8 @@
 extends Node
 
 var points = 0
+@onready var points_label: Label = $"../UI/Panel/PointsLabel"
 
 func add_point():
 	points += 1
-	print(points)
+	points_label.text = "Points: " + str(points)
